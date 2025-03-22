136f84a6a5d098c50ea35c2941f90ea6523837fc
patanyska <patriciamariafernandesnogueira@gmail.com>
2025-03-22 23:01:08 +0000

    improvement to drugbank query
    Add UNION ALL and split in three queries
    update LEFT JOINS  to INNER JOINS
    ADD final lines in each query:
    GROUP BY d.drugbank_id
    HAVING d.name IS NOT NULL
    
    Update query begin because split was wrong, since code was splitting the string and not the words

--- Bio/FoundSequence/DrugBankDataAccess.py
+++ Bio/FoundSequence/DrugBankDataAccess.py
@@ -24,93 +24,130 @@ def closeConnection(connection):
 
     Variables: - indication: is the patology or problem founded sequence
 """
-def get_drug_by_diasease(connection,diseases):
+def get_drug_by_diasease(connection,disease):
     records=[]
     try:
-        for disease in diseases:
-            strings_disease=[]
-            splited_disease=disease.split()
-            composed_disease=""
-            pos=0
-            for i in splited_disease:
-                if(pos==0):
-                    strings_disease.append(i)
-                    composed_disease=i
-                else:
-                    strings_disease.append(composed_disease+" "+i)
-                    composed_disease=composed_disease+" "+i
-                pos=pos+1
+
+        words_disease=[]
+        splited_words=disease.split(" ") #split each disease in words ex:[heart,attack]
+        composed_disease=""
+        pos=0
+        for i in splited_words:
+            if(pos==0):
+                words_disease.append(i)
+                composed_disease=i
+            else:
+                words_disease.append(composed_disease+" "+i)
+                composed_disease=composed_disease+" "+i
+            pos=pos+1
 
             
-            if(len(strings_disease)==1):
-                col_description="(d.description like '%"+strings_disease[0]+"%') COLLATE NOCASE"
-                col_indication="(d.indication like '%"+strings_disease[0]+"%') COLLATE NOCASE"
-                col_pathway="(pt.name like '%"+strings_disease[0]+"%') COLLATE NOCASE"
-            else:
-                col_description=""
-                counti=1
-                for s in strings_disease:
-                    if(counti==1):
-                        col_description=col_description +"(d.description like '%"+s+"%' OR "
+        if(len(words_disease)==1):
+            col_description="(d.description like '%"+words_disease[0]+"%') COLLATE NOCASE"
+            col_indication="(d.indication like '%"+words_disease[0]+"%') COLLATE NOCASE"
+            col_pathway="(pt.name like '%"+words_disease[0]+"%') COLLATE NOCASE"
+        else:
+            col_description=""
+            counti=1
+            for word in words_disease:
+                if(counti==1):
+                    col_description=col_description +"(d.description like '%"+word+"%' OR "
+                else:
+                    if(counti<len(words_disease)):
+                        col_description=col_description +"d.description like '%"+word+"%' OR "
                     else:
-                        if(counti<len(strings_disease)):
-                            col_description=col_description +"d.description like '%"+s+"%' OR "
-                        else:
-                            col_description=col_description +"d.description like '%"+s+"%') COLLATE NOCASE"
-                    counti=counti+1
-                        
-                col_indication=""
-                counti=1
-                for s in strings_disease:
-                    if(counti==1):
-                        col_indication=col_indication +"(d.indication like '%"+s+"%' OR "
+                        col_description=col_description +"d.description like '%"+word+"%') COLLATE NOCASE"
+                counti=counti+1
+                    
+            col_indication=""
+            counti=1
+            for word in words_disease:
+                if(counti==1):
+                    col_indication=col_indication +"(d.indication like '%"+word+"%' OR "
+                else:
+                    if(counti<len(words_disease)):
+                        col_indication=col_indication +"d.indication like '%"+word+"%' OR "
                     else:
-                        if(counti<len(strings_disease)):
-                            col_indication=col_indication +"d.indication like '%"+s+"%' OR "
-                        else:
-                            col_indication=col_indication +"d.indication like '%"+s+"%') COLLATE NOCASE"
-                    counti=counti+1
-                
-                col_pathway=""
-                counti=1
-                for s in strings_disease:
-                    if(counti==1):
-                        col_pathway=col_pathway +"(pt.name like '%"+s+"%' OR "
-                    else:
-                        if(counti<len(strings_disease)):
-                            col_pathway=col_pathway +"pt.name like '%"+s+"%' OR "
-                        else:
-                            col_pathway=col_pathway +"pt.name like '%"+s+"%') COLLATE NOCASE"
-                    counti=counti+1
+                        col_indication=col_indication +"d.indication like '%"+word+"%') COLLATE NOCASE"
+                counti=counti+1
             
-            cursor=connection.cursor()
-            sql="""SELECT distinct(d.name)            
-            ,d.drugbank_id
-            ,d.description
-            ,d.cas_number
-            ,d.unii
-            ,d.state
-            ,d.indication
-            ,d.pharmacodynamics
-            ,d.mechanism_of_action          
-            ,GROUP_CONCAT(distinct(p.name)) as product_name            
-            ,GROUP_CONCAT(distinct(p.labeller)) as labeller                         
-            ,GROUP_CONCAT(distinct(p.route)) as route                          
-            ,GROUP_CONCAT(distinct(p.country)) as country                    
-            FROM drug d 
-                LEFT JOIN groups g on d.drugbank_id=g.drugbank_id                   
-                LEFT JOIN products p on d.drugbank_id=p.drugbank_id
-                LEFT JOIN pathaway pt on d.drugbank_id=pt.drugbank_id   
-                    WHERE 
-                        ("""+col_indication+""" OR """+col_pathway+""" OR """+col_description+""") 
-                        AND p.approved='true'
-                        AND (p.ended_marketing_on IS "" OR p.ended_marketing_on>=date('now'))
-                        AND d.drugbank_id NOT IN (select drugbank_id from groups gr where gr.description like "withdrawn")"""
+            col_pathway=""
+            counti=1
+            for word in words_disease:
+                if(counti==1):
+                    col_pathway=col_pathway +"(pt.name like '%"+word+"%' OR "
+                else:
+                    if(counti<len(words_disease)):
+                        col_pathway=col_pathway +"pt.name like '%"+word+"%' OR "
+                    else:
+                        col_pathway=col_pathway +"pt.name like '%"+word+"%') COLLATE NOCASE"
+                counti=counti+1
+            
+        cursor=connection.cursor()
+        sql="""SELECT distinct(d.name)            
+        ,d.drugbank_id
+        ,d.description
+        ,d.state
+        ,d.indication
+        ,GROUP_CONCAT(distinct(p.route)) as route                          
+        ,GROUP_CONCAT(distinct(p.country)) as country                    
+        FROM drug d 
+            INNER JOIN groups g on d.drugbank_id=g.drugbank_id                   
+            INNER JOIN products p on d.drugbank_id=p.drugbank_id
+            INNER JOIN pathaway pt on d.drugbank_id=pt.drugbank_id   
+                WHERE 
+                    ("""+col_indication+""") 
+                    AND p.approved='true'
+                    AND (p.ended_marketing_on IS "" OR p.ended_marketing_on>=date('now'))
+                    AND d.drugbank_id NOT IN (select drugbank_id from groups gr where gr.description like "withdrawn")
+                GROUP BY d.drugbank_id
+                HAVING d.name IS NOT NULL
+        UNION ALL
+
+        SELECT distinct(d.name)            
+        ,d.drugbank_id
+        ,d.description
+        ,d.state
+        ,d.indication
+        ,GROUP_CONCAT(distinct(p.route)) as route                          
+        ,GROUP_CONCAT(distinct(p.country)) as country                    
+        FROM drug d 
+            INNER JOIN groups g on d.drugbank_id=g.drugbank_id                   
+            INNER JOIN products p on d.drugbank_id=p.drugbank_id
+            INNER JOIN pathaway pt on d.drugbank_id=pt.drugbank_id   
+                WHERE 
+                    ("""+col_pathway+""") 
+                    AND p.approved='true'
+                    AND (p.ended_marketing_on IS "" OR p.ended_marketing_on>=date('now'))
+                    AND d.drugbank_id NOT IN (select drugbank_id from groups gr where gr.description like "withdrawn")
+                GROUP BY d.drugbank_id
+                HAVING d.name IS NOT NULL
+        UNION ALL
+
+        SELECT distinct(d.name)            
+        ,d.drugbank_id
+        ,d.description
+        ,d.state
+        ,d.indication
+        ,GROUP_CONCAT(distinct(p.route)) as route                          
+        ,GROUP_CONCAT(distinct(p.country)) as country                    
+        FROM drug d 
+            INNER JOIN groups g on d.drugbank_id=g.drugbank_id                   
+            INNER JOIN products p on d.drugbank_id=p.drugbank_id
+            INNER JOIN pathaway pt on d.drugbank_id=pt.drugbank_id   
+                WHERE 
+                    ("""+col_description+""") 
+                    AND p.approved='true'
+                    AND (p.ended_marketing_on IS "" OR p.ended_marketing_on>=date('now'))
+                    AND d.drugbank_id NOT IN (select drugbank_id from groups gr where gr.description like "withdrawn")
+                GROUP BY d.drugbank_id
+                HAVING d.name IS NOT NULL
+        """
         
-        
-            cursor.execute(sql)
-            record = cursor.fetchall()
-            records.append(record)
+        cursor.execute(sql)
+        columns = [desc[0] for desc in cursor.description]  # Get column names
+        records.extend([dict(zip(columns, row)) for row in cursor.fetchall()])  # Convert to dict list
+
         return records
     except:
        raise ValueError(
